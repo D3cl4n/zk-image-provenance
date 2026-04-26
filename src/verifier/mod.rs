0c1cc63981c59196788662b59c661fe43a9195fa
D3cl4n <dmurphyp546@gmail.com>
2026-04-26 12:39:55 -0400

    constructing field element from hash

--- src/verifier/mod.rs
+++ src/verifier/mod.rs
@@ -25,6 +25,28 @@ fn pack_grey_pixels<F: PrimeField>(edited_img: &String) -> Vec<F> {
         .collect()
 }
 
-// extract the Poseidon(r||g||b||exif) hash from the edited image as the expected value
+// extract the Poseidon(r||g||b||exif) hash from the edited image as part of the expected value
+fn extract_hash_from_png<F: PrimeField>(edited_img: &String) -> F {
+    let image_chunks: Vec<png::PngChunk> = png::get_image_chunks(edited_img);
+
+    // extract the hash value from the chunks
+    let hash: Vec<u8> = image_chunks.iter()
+        .find(|c| &c.chunk_type == b"hASh")
+        .expect("[!] No hASh chunk found")
+        .chunk_data
+        .clone();
+
+    // pack bytes of hash into field element- reversed to match big endian
+    let mut element: F = F::ZERO;
+    let mut base: F = F::ONE;
+    let base_256: F = F::from(256u64);
+
+    for byte in hash.iter().rev() {
+        element += F::from(*byte as u64) * base;
+        base *= base_256;
+    }
+
+    element
+}
 
 // combine the hash and packed grey pixels into vector of expected field elements
