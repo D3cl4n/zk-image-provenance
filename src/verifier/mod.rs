57a1bcc144a7dd1215b164543a3ebd394e5b7992
D3cl4n <dmurphyp546@gmail.com>
2026-05-09 18:14:26 -0400

    bug fix

--- src/verifier/mod.rs
+++ src/verifier/mod.rs
@@ -110,7 +110,7 @@ fn verifying_key_from_bin(vk_bin: &String) -> PublicKey {
 fn verify_ecdsa_signature(edited_img: &String, public_key: &String) -> bool {
     println!("[*] Verifying ECDSA signature");
     let message: Message = ecdsa_message_from_digest(edited_img);
-    let secp: Secp256k1 = Secp256k1::new();
+    let secp = Secp256k1::new();
     let vk: PublicKey = verifying_key_from_bin(public_key);
 
     true
