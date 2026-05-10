f398fb649e9caa9ce9d4a1a38619c6ca92b83f8b
D3cl4n <dmurphyp546@gmail.com>
2026-05-10 11:15:33 -0400

    bug fix

--- capture.py
+++ capture.py
@@ -543,7 +543,7 @@ class Camera:
                 b_vec.append(b)
 
         exif_data = list(self.construct_exif_data())
-        raw_preimage = r_vec + g_vec + b_vec + exif_data)
+        raw_preimage = r_vec + g_vec + b_vec + exif_data
         packed_preimage = self.pack(raw_preimage)
         padded_preimage = self.pad(packed_preimage, 3)
         H = int(self.poseidon_instance.hash(padded_preimage).lift()).to_bytes(32, "big")
