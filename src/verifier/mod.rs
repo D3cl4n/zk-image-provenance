983dbc49cf0dee22d4ddf4eb3682c3bfde15d14c
D3cl4n <dmurphyp546@gmail.com>
2026-05-27 19:37:24 -0400

    removing functions

--- src/verifier/mod.rs
+++ src/verifier/mod.rs
@@ -3,51 +3,6 @@ use secp256k1::{Secp256k1, Message, PublicKey, ecdsa::{Signature}};
 use crate::png;
 
 
-// extract the grey pixels from the edited image as the expected value - pack 31 pixels per field element
-fn pack_grey_pixels<F: PrimeField>(edited_img: &String) -> Vec<F> {
-    let bytes_per_element: usize = 31;
-    let grey_pixels: Vec<u8> = png::get_png_greyscale_values(edited_img);
-    
-    grey_pixels
-        .chunks(bytes_per_element)
-        .map(|chunk| {
-            let mut element: F = F::ZERO;
-            let base_256: F = F::from(256 as u64);
-
-            // iterate over each byte in slice and pack into position based on powers of 256
-            for &byte in chunk {
-                element = element * base_256 + F::from(byte as u64);
-            } 
-
-            element
-        })
-        .collect()
-}
-
-// extract the Poseidon(r||g||b||exif) hash from the edited image as part of the expected value
-fn extract_hash_from_png<F: PrimeField>(edited_img: &String) -> F {
-    let image_chunks: Vec<png::PngChunk> = png::get_image_chunks(edited_img);
-
-    // extract the hash value from the chunks
-    let hash: Vec<u8> = image_chunks.iter()
-        .find(|c| &c.chunk_type == b"hASh")
-        .expect("[!] No hASh chunk found")
-        .chunk_data
-        .clone();
-
-    // pack bytes of hash into field element- reversed to match big endian
-    let mut element: F = F::ZERO;
-    let mut base: F = F::ONE;
-    let base_256: F = F::from(256u64);
-
-    for byte in hash.iter().rev() {
-        element += F::from(*byte as u64) * base;
-        base *= base_256;
-    }
-
-    element
-}
-
 // pack a vector of u8s into field elements
 fn pack_into_field_elements<F: PrimeField>(data: Vec<u8>) -> Vec<F> {
     let bytes_per_element: usize = 31;
@@ -92,15 +47,9 @@ pub fn construct_expected_value<F: PrimeField>(edited_img: &String) -> Vec<F> {
 // extract the bytes of the ECDSA signature from the edited png
 fn extract_signature_from_png(edited_img: &String) -> Signature {
     println!("[*] Extracting ECDSA signature from edited png");
-    let image_chunks: Vec<png::PngChunk> = png::get_image_chunks(edited_img);
 
     // extract the hash value from the chunks
-    let signature: Vec<u8> = image_chunks.iter()
-        .find(|c| &c.chunk_type == b"sIGn")
-        .expect("[!] No sIGn chunk found")
-        .chunk_data
-        .clone();
-
+    let signature: Vec<u8> = extract_chunk_data(edited_img, b"sIGn");
     let signature_bytes: [u8; 64] = signature.try_into().expect("[!] Vector is not 64 elements long");
 
     Signature::from_compact(&signature_bytes).expect("[!] Signature is incorrect length")
