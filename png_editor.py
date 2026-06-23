7862549562bcf0e23ef41409acf0c5bb3d483bb1
D3cl4n <dmurphyp546@gmail.com>
2026-06-23 12:45:42 -0400

    more benchmarking

--- png_editor.py
+++ png_editor.py
@@ -112,7 +112,7 @@ class EditorUtils:
 
 # main function
 def main():
-    editor = EditorUtils("original.png")
+    editor = EditorUtils("original_256.png")
     editor.greyscale()
 
 
--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
     use rand::rngs::OsRng;
 
     // original image as private witness, edited image as public, verifying key is public but used off-circuit
-    let original_img = String::from("original.png");
+    let original_img = String::from("original_256.png");
     let edited_img = String::from("greyscale.png");
     let public_key = String::from("verifying_key.bin");
 
@@ -29,7 +29,7 @@ fn main() {
 
     // setup the proof generation
     let circuit = prover::construct_circuit_struct(&original_img);
-    let k: u32 = 15;
+    let k: u32 = 19;
     let params: Params<EqAffine> = Params::new(k);
     let vk = keygen_vk(&params, &circuit).expect("[!] keygen_vk failed");
     let pk = keygen_pk(&params, vk.clone(), &circuit).expect("[!] keygen_pk failed");
