4c434db0d95b2ac23411789a02f082f54a1db65d
D3cl4n <dmurphyp546@gmail.com>
2026-07-01 19:39:20 -0400

    collision probabiliy calculations

---
+++ collision_probability.py
@@ -0,0 +1,39 @@
+import math
+
+
+# find the distribution of greyscale values across the space [0-256)
+def count():
+    y_distribution = [0] * 256
+    
+    # iterate over all the rgb pixel channel combinations
+    for r in range(256):
+        for g in range(256):
+            for b in range(256):
+                y = math.floor((30*r + 58*g + 11*b)/100)
+                y_distribution[y] += 1
+
+    return y_distribution
+
+
+# find the number of colliding pairs without repeating order (A, B) = (B, A) here
+def count_colliding_pairs(y_distribution):
+    total = 0
+    for y in range(256):
+        count_y = y_distribution[y]
+        total += (count_y * (count_y - 1)) 
+
+    return total
+
+
+# main function
+def main():
+    N = 256**3
+    distribution = count()
+    colliding_pairs = count_colliding_pairs(distribution)
+    prob = colliding_pairs / (N*(N-1))
+
+    print(prob)
+
+
+if __name__ == '__main__':
+    main()
