9802de72d545a0b799a1b9da4051d7e70c7e523c
D3cl4n <dmurphyp546@gmail.com>
2026-07-07 07:38:38 -0400

    need to debug constraint failure

--- png_editor.py
+++ png_editor.py
@@ -112,7 +112,7 @@ class EditorUtils:
 
 # main function
 def main():
-    editor = EditorUtils("photos/original_256.png")
+    editor = EditorUtils("photos_unpacked/original_64.png")
     editor.greyscale()
 
 
--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,8 @@ fn main() {
     use rand::rngs::OsRng;
 
     // original image as private witness, edited image as public, verifying key is public but used off-circuit
-    let original_img = String::from("photos/original_256.png");
-    let edited_img = String::from("photos/greyscale.png");
+    let original_img = String::from("photos_unpacked/original_64.png");
+    let edited_img = String::from("photos_unpacked/greyscale.png");
     let public_key = String::from("verifying_key.bin");
 
     // verifier functionality off-circuit and prover circuit struct creation
@@ -29,7 +29,7 @@ fn main() {
 
     // setup the proof generation
     let circuit = prover::construct_circuit_struct(&original_img);
-    let k: u32 = 19;
+    let k: u32 = 15;
     let params: Params<EqAffine> = Params::new(k);
     let vk = keygen_vk(&params, &circuit).expect("[!] keygen_vk failed");
     let pk = keygen_pk(&params, vk.clone(), &circuit).expect("[!] keygen_pk failed");
