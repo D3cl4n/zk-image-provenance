a898b2b3e9b29d102d685178ef49bd11070a95cc
D3cl4n <dmurphyp546@gmail.com>
2026-07-10 15:13:18 -0400

    debugging

--- src/verifier/mod.rs
+++ src/verifier/mod.rs
@@ -25,20 +25,10 @@ pub fn construct_expected_value<F: PrimeField>(edited_img: &String) -> Vec<F> {
         png::extract_chunk_data(edited_img, b"eXIf")
     ));
     
-    let hash_bytes: Vec<u8> = png::extract_chunk_data(edited_img, b"hASh");
-    
-    // pack bytes of hash into field element- reversed to match big endian
-    let mut element: F = F::ZERO;
-    let mut base: F = F::ONE;
-    let base_256: F = F::from(256u64);
+    expected.extend(convert_into_field_elements::<F>(
+        png::extract_chunk_data(edited_img, b"hASh")
+    ));
 
-    for byte in hash_bytes.iter().rev() {
-        element += F::from(*byte as u64) * base;
-        base *= base_256;
-    }
-
-    expected.push(element);
-    println!("[*] Verifier expected hash {:?}", element);
     println!("[*] Verifier expected len {:?}", expected.len());
 
     expected
